b0b3bed39494e93b9262c74f804678d4386bef3f
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-09-14 16:46:57 +0300

    setting gh-deploy

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
+  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: false,
   swcMinify: true,
 };
