09e1c1b8f17c85ec5197d8b101915087677379b4
Makar Korolenko <80404689+Corvus278@users.noreply.github.com>
2022-09-14 17:05:55 +0300

    Edit repo name

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
-  assetPrefix: isProd ? "/your-github-repo-name/" : "",
+  assetPrefix: isProd ? "/Tic-tac-toe/" : "",
   images: {
     unoptimized: true,
   },
