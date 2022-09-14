56a228978636c7f6cbbcc69ca727e9c9beb6f943
Corvus278 <Corvus278@users.noreply.github.com>
2022-09-14 14:00:45 +0000

    deploy: ae91779e197ab45d55b631c0b9b1364cf819d90d

---
+++ _next/static/vFbNYJtLiEzPTJrQ9eFL1/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/5ce55f3c62b5f13e.css","static/chunks/pages/index-04fc6c31c9204502.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
